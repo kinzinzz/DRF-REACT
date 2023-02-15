f793bc50f8b83574fc888be62d4514d224647a57
LEE DONG YEONG <kinzinzz@naver.com>
2023-02-15 23:14:06 +0900

    JWT + login, logout

--- REACT/blogapi/src/index.js
+++ REACT/blogapi/src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as serviceWorker from './serviceWorker.js';
 import './index.css';
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import App from './App';
-import Header from './components/Header';
-import Footer from './components/Footer';
+import Header from './components/header';
+import Footer from './components/footer';
+import Register from './components/register';
+import Login from './components/login';
+import Logout from './components/logout';
 
 const routing = (
   <Router>
@@ -12,6 +16,9 @@ const routing = (
       <Header />
       <Switch>
         <Route exact path="/" component={App} />
+        <Route path="/register" component={Register} />
+        <Route path="/login" component={Login} />
+        <Route path="/logout" component={Logout} />
       </Switch>
       <Footer />
     </React.StrictMode>
@@ -23,3 +30,4 @@ ReactDOM.render(routing, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
